b9d8eb33f842b256092f565f04115bae9bd4b264
KARAN SHARMA <karansharma032000@gmail.com>
2020-07-23 16:59:58 -0400

    Lab 7 commit

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,21 +13,31 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ChatRoomActivity extends AppCompatActivity {
+
+    AppCompatActivity parentActivity;
     private ArrayList<Message>  messages = new ArrayList<>();
     private Button sendbtn, receivebtn;
     private EditText txt;
     private ListView msgList;
     public static final String ACTIVITY_NAME = "CHAT_ROOM_ACTIVITY";
+
+    public static final String ITEM_SELECTED = "ITEM";
+    public static final String ITEM_MESSAGE = "MESSAGE";
+    public static final String ITEM_ID = "ID";
+    public static final String ITEM_ISSEND = "ISSEND";
+
     Message msg;
-    SQLiteDatabase db;
-    ContentValues cv = new ContentValues();
+//    SQLiteDatabase db;
+//    ContentValues cv = new ContentValues();
     MyListAdapter adpter = new MyListAdapter();
     MyOpener dbopen = new MyOpener(this);
+    Boolean isTablet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +52,9 @@ public class ChatRoomActivity extends AppCompatActivity {
         receivebtn = findViewById(R.id.receivebtn);
         txt = findViewById(R.id.input);
 
+        isTablet = findViewById(R.id.fragmentLocation) !=null; // to  check if frame layout is loaded
+        TextView getmessage = findViewById(R.id.messageView);/////////////////////////////////////////////////
+
         msgList.setAdapter(adpter);
 
         if(dbopen.count()>0){
@@ -54,11 +68,43 @@ public class ChatRoomActivity extends AppCompatActivity {
             txt.setText("");
         });
 
+        msgList.setOnItemClickListener((parent, view, position, id) -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLocation, new DetailsFragment()).commit();
+        });
+
         receivebtn.setOnClickListener(click-> {
             Message message = new Message(txt.getText().toString(), false);
             adpter.setMessage(message);
             dbopen.insertMessage(message);
             txt.setText("");
+        });
+
+        msgList.setOnItemClickListener((list, view, position, id) -> {
+            //Create a bundle to pass data to the new fragment
+            Bundle dataToPass = new Bundle();
+            dataToPass.putString(ITEM_SELECTED, String.valueOf(messages.get(position)));
+//            dataToPass.putString(ITEM_MESSAGE, (String) getmessage.getText());  * Not working
+            dataToPass.putLong(ITEM_ID, id);
+            dataToPass.putBoolean(ITEM_ISSEND, false);
+
+            if(isTablet)
+            {
+                DetailsFragment dFragment = new DetailsFragment(); //add a DetailFragment
+                dFragment.setArguments( dataToPass ); //pass it a bundle for information
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentLocation, dFragment) //Add the fragment in FrameLayout
+                        .commit(); //actually load the fragment. Calls onCreate() in DetailFragment
+            }
+            else //isPhone
+            {
+                Intent nextActivity = new Intent(this, EmptyActivity.class);
+                nextActivity.putExtras(dataToPass); //send data to next activity
+                startActivity(nextActivity); //make the transition
+            }
+
+
+
         });
 
         msgList.setOnItemLongClickListener((parent, view, position, id) ->{
---
+++ app/src/main/java/com/example/androidlabs/EmptyActivity.java
@@ -0,0 +1,24 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+
+        Bundle dataToPass = getIntent().getExtras();
+
+        DetailsFragment dFragment = new DetailsFragment(); //add a DetailFragment
+        dFragment.setArguments( dataToPass ); //pass it a bundle for information
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentLocation, dFragment) //Add the fragment in FrameLayout
+                .commit(); //actually load the fragment. Calls onCreate() in DetailFragment
+    }
+}
